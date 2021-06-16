e800cdba691c41e92665e29f2f7bb926245ea318
takker99 <37929109+takker99@users.noreply.github.com>
2021-06-16 11:39:24 +0900

    :white_check_mark: move tests to the documentation comment and add the test for wildcard

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,63 @@ impl Asearch {
     ///
     /// - `text` - text which is searched.
     /// - `ambig` - Levenshtein distance.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// // pattern: abcde
+    /// {
+    ///     let asearch = asearch::Asearch::new("abcde");
+    ///
+    ///     assert!(asearch.find("abcde", 0));
+    ///     assert!(asearch.find("aBCDe", 0));
+    ///     assert!(asearch.find("abXcde", 1));
+    ///     assert!(asearch.find("ab?de", 1));
+    ///     assert!(asearch.find("abXXde", 2));
+    ///     assert!(!asearch.find("abXcde", 0));
+    ///     assert!(!asearch.find("ab?de", 0));
+    ///     assert!(!asearch.find("abde", 0));
+    ///     assert!(!asearch.find("abXXde", 1));
+    ///     assert!(asearch.find("abcde", 1));
+    ///     assert!(!asearch.find("abcd", 0));
+    ///     assert!(asearch.find("abcd", 1));
+    ///     assert!(asearch.find("bcde", 2));
+    /// }
+    ///
+    /// // pattern: ab de
+    /// {
+    ///     let asearch = asearch::Asearch::new("ab de");
+    ///     assert!(asearch.find("abcde", 0));
+    ///     assert!(asearch.find("abccde", 0));
+    ///     assert!(asearch.find("abXXXXXXXde", 0));
+    ///     assert!(asearch.find("ababcccccxede", 1));
+    ///     assert!(!asearch.find("abcccccxe", 0));
+    /// }
+    ///
+    /// // pattern: partial match
+    /// {
+    ///     let asearch = asearch::Asearch::new(" java ");
+    ///
+    ///     assert!(asearch.find("javascript", 0));
+    ///     assert!(asearch.find("abcjavascript", 0));
+    ///     assert!(asearch.find("jabascript", 1));
+    ///     assert!(asearch.find("awesome jabascript", 1));
+    /// }
+    ///
+    /// // pattern: unicode
+    /// {
+    ///     let asearch = asearch::Asearch::new("漢字文字列");
+    ///
+    ///     assert!(asearch.find("漢字文字列", 0));
+    ///     assert!(!asearch.find("漢字の文字列", 0));
+    ///     assert!(asearch.find("漢字の文字列", 1));
+    ///     assert!(!asearch.find("漢字文字", 0));
+    ///     assert!(asearch.find("漢字文字", 1));
+    ///     assert!(!asearch.find("漢字文字烈", 0));
+    ///     assert!(asearch.find("漢字文字烈", 1));
+    ///     assert!(!asearch.find("漢和辞典", 2));
+    /// }
+    /// ```
     pub fn find(&self, text: impl Into<String>, ambig: u8) -> bool {
         let ambig_ = if (ambig as usize) < INITSTATE.len() {
             ambig as usize
@@ -104,54 +161,5 @@ fn to_upper(c: usize) -> usize {
         c - 0x20
     } else {
         c
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn pattern_abcde() {
-        let asearch = Asearch::new("abcde");
-
-        assert!(asearch.find("abcde", 0));
-        assert!(asearch.find("aBCDe", 0));
-        assert!(asearch.find("abXcde", 1));
-        assert!(asearch.find("ab?de", 1));
-        assert!(asearch.find("abXXde", 2));
-        assert!(!asearch.find("abXcde", 0));
-        assert!(!asearch.find("ab?de", 0));
-        assert!(!asearch.find("abde", 0));
-        assert!(!asearch.find("abXXde", 1));
-        assert!(asearch.find("abcde", 1));
-        assert!(!asearch.find("abcd", 0));
-        assert!(asearch.find("abcd", 1));
-        assert!(asearch.find("bcde", 2)); // TODO: 1で通るようにcodeを修正する
-    }
-
-    #[test]
-    fn pattern_ab_de() {
-        let asearch = Asearch::new("ab de");
-
-        assert!(asearch.find("abcde", 0));
-        assert!(asearch.find("abccde", 0));
-        assert!(asearch.find("abXXXXXXXde", 0));
-        assert!(asearch.find("ababcccccxede", 1));
-        assert!(!asearch.find("abcccccxe", 0));
-    }
-
-    #[test]
-    fn pattern_unicode() {
-        let asearch = Asearch::new("漢字文字列");
-
-        assert!(asearch.find("漢字文字列", 0));
-        assert!(!asearch.find("漢字の文字列", 0));
-        assert!(asearch.find("漢字の文字列", 1));
-        assert!(!asearch.find("漢字文字", 0));
-        assert!(asearch.find("漢字文字", 1));
-        assert!(!asearch.find("漢字文字烈", 0));
-        assert!(asearch.find("漢字文字烈", 1));
-        assert!(!asearch.find("漢和辞典", 2));
     }
 }
